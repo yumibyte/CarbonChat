c14e59bed4dd19c024f43f5edc66b17304644964
Edward Raigosa <wenlock@github.com>
2020-10-03 20:24:20 -0700

    typo

--- script/build-web.sh
+++ script/build-web.sh
@@ -4,7 +4,7 @@ set -e
 
 [[ ! -d .git ]] && (>&2 echo "ERROR Please run this script from the root" && exit 1)
 
-source './web/.envpp'
+source './web/.envapp'
 
 docker build -t "${DOCKER_REGISTRY}/${REPO_PATH}/web:latest" \
              -f web/Dockerfile ./web
