32436c4358ee2c575ae289765362d1c2196452c1
Yumibyte <ashraigosa@gmail.com>
2020-10-04 21:04:32 -0700

    changes for specific time and co2 at end of excel file

--- MergeDatasets/NC4Merge.py
+++ MergeDatasets/NC4Merge.py
@@ -47,8 +47,6 @@ path = os.getcwd()
 # retrieve all files in directory and get rid of py file in it (this file)
 file_list = os.listdir(path)
 file_list.remove("NC4Merge.py")
-InfoDF = pd.DataFrame()
-#print(ds['date'])
 
 datesList = []
 longitudeList = []
@@ -63,11 +61,14 @@ yearsList = []
 monthsList = []
 daysList = []
 hoursList = []
+minutesList = []
+secondsList = []
 
 # retrieve values for date, xco2, lat, long, uncertainty, and location for each 30,000 piece of data in each file in this directory
 for file in file_list:
     ds = nc.Dataset(file)
     date = ds['date'][:]
+
     longitude = ds['longitude'][:]
     latitude = ds['latitude'][:]
     xco2 = ds['xco2'][:]
@@ -79,6 +80,8 @@ for file in file_list:
         latitudeList.append(latitude[i])
         xco2List.append(xco2[i])
         uncertaintyList.append(uncertainty[i])
+
+print(ds['date'])
 
 # discover location
 locationsList = []
@@ -102,15 +105,17 @@ for date in datesList:
     monthsList.append(date[1])
     daysList.append(date[2])
     hoursList.append(date[3])
+    minutesList.append(date[4])
+    secondsList.append(date[5])
 
-# combine all lists into one list to be zipped 
-finalDataList = tuple(zip(yearsList, monthsList, daysList, hoursList, xco2List, localityList, largerAreaList, countryList, latitudeList, longitudeList, uncertaintyList))
+# combine all lists into one list to be zipped
+finalDataList = tuple(zip(yearsList, monthsList, daysList, hoursList, minutesList, secondsList, localityList, largerAreaList, countryList, latitudeList, longitudeList, uncertaintyList, xco2List))
 print(finalDataList)
 
 # write to excel file
 
 def Write_xlsx(fileName, data):
-    DB_columns = ['year', 'month', 'day', 'hour', 'CO2', 'locality', 'largerArea', 'country', 'lat', 'long', 'uncertainty']
+    DB_columns = ['year', 'month', 'day', 'hour', 'minute', 'second', 'locality', 'largerArea', 'country', 'lat', 'long', 'uncertainty', 'CO2']
 
     df = DataFrame(data, columns=DB_columns)
     df.to_excel(fileName, index=False)
