367b5a0f0438f8167f72c9979aa048eb295ac931
Edward Raigosa <wenlock@github.com>
2020-10-04 19:58:21 -0700

    remove hello world

--- CarbonChatApp/CarbonChatApp/ContentView.swift
+++ CarbonChatApp/CarbonChatApp/ContentView.swift
@@ -184,8 +184,3 @@ struct ContentView: View {
 }
 
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
-    }
-}
